c416e96fd8a2d2a6b3735932d888fcc45786ce70
edgar <harove@gmail.com>
2019-05-16 17:08:21 -0400

    desafio arrays

---
+++ desafioArrays.rb
@@ -0,0 +1,45 @@
+# arreglo = [1,2,3,9,1,4,5,2,3,6,6]
+#puts arreglo[0]
+#puts arreglo[-1]
+#arreglo.each {|i| puts i}
+#arreglo.each_with_index {|elem , index| puts "#{index})  #{elem}"}
+
+# arreglo.each_with_index {|elem , index| puts "#{index})  #{elem}" if index.even?}
+
+#2
+a = [1,2,3,9,1,4,5,2,3,6,8]
+# print a
+# a.pop
+# print a 
+# print a
+# a.shift
+# print a
+
+# print a
+# puts
+# a.delete_at((a.length/2)-1)
+# print a
+
+# print a
+# a.pop if a[-1] != 1
+# print a 
+# aux=[]
+# print a
+# 0.upto (a.size-1) do
+#     aux.push(a.pop)
+# end
+# print aux
+
+3.1#
+
+# a = [1,2,3,9,1,4,5,2,3,6,8]
+# print a
+# puts
+#     index=0
+#     while index < a.length do
+#         a[index].even? ? a.delete_at(index) : index+=1
+#     end
+# print a
+# puts
+
+
